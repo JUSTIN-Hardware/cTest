47871a38bb81d1baeb1e50b3bb214c2752f1a41e
JUSTIN-Hardware <justin518366@gmail.com>
2025-06-26 11:59:00 +0900

    new

--- teset.c
+++ teset.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 
+void double_values(int *arr, int size);
+
 int main()
 {
+    int arr[5] = {1,2,3,4,5};
 
-    printf("helloworld");
+    double_values(arr,5);
+
+    printf("배열 값 2배 결과 : \n");
+    for(int i=0; i<5; i++)
+    {
+        printf("%d", arr[i]);
+    }
+
+    printf("\n");
+
     return 0;
-    
+}
+
+
+
+void double_values(int *arr, int size)
+{
+    for (int i =0; i<size; i++)
+    {
+        arr[i] *=2;
+    }
 }

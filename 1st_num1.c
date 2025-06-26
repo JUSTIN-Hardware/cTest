f70c1ada30e2c33422059fcdd6320db69c79f383
JUSTIN-Hardware <justin518366@gmail.com>
2025-06-26 14:37:23 +0900

    example 1

--- 1st_num1.c
+++ 1st_num1.c
@@ -3,5 +3,16 @@
 
 int main()
 {
+    unsigned char rx_buffer[11] = {0x49, 0x20, 0x43, 0x41, 0x4E, 0x20, 0x44, 0x4f, 0x20, 0x49, 0x54}; // I CAN DO IT
+
+    printf("reveiced data :");
+    for(int i =0; i<12; i++){
+        printf("%c",rx_buffer[i]);   
+    }
+    printf("\n");
+
     return 0;
 }
+
+
+

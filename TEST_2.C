fff8a588902f9c28f95e7910e9de7b803ec35b41
JUSTIN-Hardware <justin518366@gmail.com>
2025-06-26 16:16:37 +0900

    new

--- TEST_2.C
+++ TEST_2.C
@@ -3,24 +3,58 @@
 #define BUFFER_SIZE 10
 
 int get_array_num(int arr[], int size);
-
+void array_input(int array[], int array_size);
 
 
 int main()
 {
-    int rx_buffer[BUFFER_SIZE] = {10,20,30,40,50,60,70,80,90,100}; 
+   
+    int array[BUFFER_SIZE];
+    int size = 0;
     int sum = 0;
     int averge = 0;
+   
+    printf("array size input(max %d)",BUFFER_SIZE);
+    scanf("%d",&size);
 
-    sum = get_array_num(rx_buffer,BUFFER_SIZE);
-    averge = sum/10;
+    if(size>BUFFER_SIZE|| size<=0)
+    {
+    printf("error : array size fix 1~%d.\n",BUFFER_SIZE);
+    return 1;
+    }
+
+    array_input(array,size);
+
+    sum = get_array_num(array,size);
+
+    averge = sum/size;
 
     printf("buffer sum :%d\n",sum);
     printf("buffer averge :%d\n",averge);
   
     return 0;
+   
+
 }
 
+
+void array_input(int array[],int array_size)
+{
+   
+    printf("input array's size : %d\n",array_size);
+    for(int i=0; i<array_size; i++)
+    {
+        scanf("%d",&array[i]);
+    }
+
+    printf("inputed array value : ");
+    for(int i=0; i < array_size; i++)
+    {
+        printf("%d ",array[i]);
+    }
+    printf("\n");
+
+}
 
 int get_array_num(int arr[], int size)
 {
